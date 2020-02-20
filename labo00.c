e65a1d469efd93035cd243a9d4b1d1044c1ed0cc
patrickfluck <61149046+patrickfluck@users.noreply.github.com>
2020-02-20 15:15:21 +0100

    Update labo00.c
    1ère partie de labo ok

--- labo00.c
+++ labo00.c
@@ -1,1 +1,16 @@
 // labo00.c Patrick Fluck
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+int main(int argc, const char* argv[])
+{
+printf("%d\n", argc);
+
+for(int8_t i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);	
+}
+return argc;			
+}
